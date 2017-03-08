e6da67e1365d7c717f408a34d8bd491e33e94388
Steeve Postemus <spostemus@gmail.com>
2017-03-08 11:12:08 -0800

    checkpoint 3 active tasks completed

--- dist/scripts/controllers/MainCtrl.js
+++ dist/scripts/controllers/MainCtrl.js
@@ -2,6 +2,29 @@
     function MainCtrl($firebaseArray) {
         var tasksRef = firebase.database().ref().child("tasks");
         this.tasks = $firebaseArray(tasksRef);
+        
+        var self = this
+        
+        this.tasks.$loaded()
+            .then(function(){
+                self.tasks.forEach(function(task){
+                    var currentTime = new Date();
+                    console.log("current time is:" + currentTime);
+                    console.log("task created at:" + task.createdAt);
+                    console.log("current time getTime is:" + currentTime.getTime())
+                }) 
+            })
+        
+        this.addTask = function(){
+            var taskObj = 
+                {   name: this.newTask,
+                    completed: false,
+                    expired: false,
+                    createdAt: firebase.database.ServerValue.TIMESTAMP
+                };
+            this.tasks.$add(taskObj);
+            this.newTask = "";
+        }
     }
 
     angular.module('blocitoff')
